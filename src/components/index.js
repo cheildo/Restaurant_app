0cc749a57994a4da708ec389ea7219432055d1b3
Chek 紀達 <cheickzida@gmail.com>
2023-04-18 17:22:43 +0800

    Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Navbar from './Navbar/Navbar';
+import Newsletter from './Footer/Newsletter';
+import FooterOverlay from './Footer/FooterOverlay';
+import MenuItem from './MenuItem/MenuItem';
+import SubHeading from './SubHeading/SubHeading';
+
+export { Navbar, Newsletter,FooterOverlay, MenuItem, SubHeading };
---
+++ src/container/index.js
@@ -0,0 +1,11 @@
+import AboutUs from './AboutUs/AboutUs.jsx';
+import Chef from './Chef/Chef.jsx';
+import Findus from './Findus/Findus.jsx';
+import Footer from './Footer/Footer.jsx';
+import Header from './Header/Header.jsx';
+import Intro from './Intro/Intro.jsx';
+import Gallery from './Gallery/Gallery.jsx';
+import Menu from './Menu/Menu.jsx';
+import Laurels from './Laurels/Laurels.jsx';
+
+export { AboutUs, Header, Chef, Findus, Footer, Intro, Gallery, Menu, Laurels };
