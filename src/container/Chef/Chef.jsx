a5238c9d3c7392a790d5e238fe67d941429fad80
Chek 紀達 <cheickzida@gmail.com>
2023-04-28 20:18:47 +0800

    Up to Chef

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import './Chef.css'
+import { images } from '../../constant/index'
+import { SubHeading } from '../../components';
 
 const Chef = () => {
   return (
-    <div>Chef</div>
+    <div className='app__chef section__padding'>
+      <div className="app__chef-image">
+        <img src={images.chef} alt="" />
+      </div>
+      <div className="app__chef-text">
+        <SubHeading title="Chef’s Word" />
+        <h1>What we believe in</h1>
+        <p className= "p__opensans">auctor sit iaculis in arcu. Vulputate nulla lobortis mauris eget sit. Nulla scelerisque scelerisque congue ac consequat, aliquam molestie lectus eu. Congue iaculis integer curabitur semper sit nunc..</p>
+        <div className="app__chef-text-sign">
+          <p className="app__chef-text-sign-name">Kevin Luo</p>
+          <p className="p__opensans">Chef & Founder</p>
+        </div>
+        <img  className="signature_image" src={images.sign} alt="Signature" />
+      </div>
+    </div>
   )
 }
 
